5470dc0f350fce11b4ba8d8cf31d866ebc51624b
Atish Shanbhag <atishshanbhag03@gmail.com>
2022-08-20 20:06:29 +0530

    ft_Programms

---
+++ Basics/traingle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    cout<<"|____|"<< endl;
+    cout<<"  / |"<< endl;
+    cout<<" /  |"<< endl;
+    cout<<"/___|"<< endl;
+
+return 0;
+}
